9c7258b445a64e09f65b82c1810487f59ee9f47f
Pawel <pawelpuszcz@gmail.com>
2022-06-22 14:37:05 +0200

    modify

--- 01_intro/01_simple_map_reduce.py
+++ 01_intro/01_simple_map_reduce.py
@@ -3,6 +3,10 @@ from mrjob.job import MRJob
 class MRWordCount(MRJob):
     def mapper(self, _, line):
         yield 'chars', len(line)
+        yield 'words', len(line.split())
+
+    def reducer(self, key, values):
+        yield key, sum(values)
 
 if __name__ == '__main__':
     MRWordCount.run()
